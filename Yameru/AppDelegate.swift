aab24e2ea721a51db4409392cb0b2e9a2ff9db0c
Victor Ingman <viregistermail@gmail.com>
2020-03-02 19:37:37 +0100

    Added working pref panel........

--- Yameru/AppDelegate.swift
+++ Yameru/AppDelegate.swift
@@ -19,32 +19,18 @@ extension PreferencePane.Identifier {
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     var window: NSWindow!
+    
+    
     lazy var preferencesWindowController = PreferencesWindowController(
         preferencePanes: [
-            SettingsGeneralViewController()
-        ]
+            PrefGeneralVC()
+        ],
+        hidesToolbarForSingleItem: false
     )
+    
 
     @IBAction func preferencesDidActivate(_ sender: Any) {
         preferencesWindowController.show()
     }
-    //    func applicationDidFinishLaunching(_ aNotification: Notification) {
-//        // Create the SwiftUI view that provides the window contents.
-//        let contentView = ContentView()
-//
-//        // Create the window and set the content view.
-//        window = NSWindow(
-//            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-//            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-//            backing: .buffered, defer: false)
-//        window.center()
-//        window.setFrameAutosaveName("Yameru")
-//        window.contentView = NSHostingView(rootView: contentView)
-//        window.makeKeyAndOrderFront(nil)
-//    }
-
-//    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-//    }
 }
 
--- Yameru/Settings/PrefGeneralVC.swift
+++ Yameru/Settings/PrefGeneralVC.swift
@@ -1,27 +1,24 @@
 //
-//  SettingsGeneralViewController.swift
+//  PrefGeneralVC.swift
 //  Yameru
 //
 //  Created by Victor Ingman on 2020-03-02.
 //  Copyright © 2020 Sun Labs. All rights reserved.
 //
 
-import Foundation
-import Cocoa
 import Preferences
+import Cocoa
 
-class SettingsGeneralViewController: NSViewController, PreferencePane {
-    
+class PrefGeneralVC: NSViewController, PreferencePane {
     let preferencePaneIdentifier = PreferencePane.Identifier.general
     let preferencePaneTitle = "General"
     let toolbarItemIcon = NSImage(named: NSImage.preferencesGeneralName)!
 
-    override var nibName: NSNib.Name? { "SettingsGeneral" }
-
+    override var nibName: NSNib.Name? { "PrefGeneralView" }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Setup stuff here
     }
-
     
 }
