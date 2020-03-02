2d365245ab85cb67d4b6aa507537594e66f67636
frosteryd <linus.frosteryd@gmail.com>
2020-03-02 15:43:22 +0100

    adding user userdefaults at init of app

--- Yameru/SettingsGeneralViewController.swift
+++ Yameru/SettingsGeneralViewController.swift
@@ -15,13 +15,13 @@ class SettingsGeneralViewController: NSViewController, PreferencePane {
     let preferencePaneIdentifier = PreferencePane.Identifier.general
     let preferencePaneTitle = "General"
     let toolbarItemIcon = NSImage(named: NSImage.preferencesGeneralName)!
-    
+
     override var nibName: NSNib.Name? { "SettingsGeneral" }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Setup stuff here
     }
+
     
 }
