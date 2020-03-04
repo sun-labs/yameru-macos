aaac04939d448f99aa862995db20c055832fba43
Victor Ingman <viregistermail@gmail.com>
2020-03-04 15:56:35 +0100

    Function for getting formatted USB devices

--- Yameru/YameruTheProtector.swift
+++ Yameru/YameruTheProtector.swift
@@ -7,7 +7,11 @@
 //
 
 import Foundation
-import IOUSBHost
+
+struct USBDevice {
+    var name: String
+    var at: String
+}
 
 class YameruTheProtector {
     /// credits: https://stackoverflow.com/questions/26971240/how-do-i-run-an-terminal-command-in-a-swift-script-e-g-xcodebuild
@@ -28,8 +32,51 @@ class YameruTheProtector {
     func lockComputer () -> String {
         return self.shell("/System/Library/CoreServices/Menu\\ Extras/User.menu/Contents/Resources/CGSession -suspend")
     }
-    func getUSBDevices () -> String {
-        let spOutput = self.shell("system_profiler SPUSBDataType")
-        return spOutput
+    
+    
+    
+    func getUSBDevices () -> [Any] {
+        let spOutput = self.shell("ioreg -p IOUSB")
+        let replaced = spOutput
+            .replacingOccurrences(of: "+-o", with: "")
+            .replacingOccurrences(of: "\t", with: "")
+            .replacingOccurrences(of: "|", with: "")
+        let lines = replaced.split(separator: "\n")
+        let cleanLines = lines.map { $0.trimmingCharacters(in: .whitespacesAndNewlines) }
+        var devices: [[String: String]] = []
+        for line in cleanLines { // for each device
+            var device: [String: String] = [:]
+            let infos = line.components(separatedBy: " <")
+            let nameAndLoc = infos[0]
+                .trimmingCharacters(in: .whitespacesAndNewlines)
+                .split(separator: "@")
+            print(nameAndLoc)
+            let name = String(nameAndLoc[0])
+            if (name == "Root") { // root controller not needed, skip
+                continue
+            }
+            let location = String(nameAndLoc[1])
+            device["name"] = name
+            device["location"] = location
+            let details = infos[1]
+                .split(separator: ",")
+                .map {
+                    $0
+                    .replacingOccurrences(of: ">", with: "")
+                    .trimmingCharacters(in: .whitespacesAndNewlines)
+                }
+            for detail in details { // for each info
+                if detail.contains(" ") {
+                    let keyVal = detail.split(separator: " ", maxSplits: 1, omittingEmptySubsequences: true)
+                    let key = String(keyVal[0])
+                    let val = String(keyVal[1])
+                    device[key] = val
+                } else {
+                    device[detail] = "true"
+                }
+            }
+            devices.append(device)
+        }
+        return devices
     }
 }
